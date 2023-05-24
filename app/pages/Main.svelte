8293983d1c4329b6b4ee2489bd65b99b2d68ac88
florentazd <florent4real@yahoo.com>
2023-05-24 14:33:30 +0000

    Add new splash screen and icon

--- app/pages/Main.svelte
+++ app/pages/Main.svelte
@@ -1,14 +1,10 @@
 <script>
 	import Screen from "./components/Screen.svelte"
 	import Buttons from "./components/Buttons.svelte"
-	import { setContext, getContext } from 'svelte';
-
-	/*setContext("result", getContext("result"))*/
-
 </script>
 <page actionBarHidden="true" statusBarStyle="dark" androidStatusBarBackground="#151515">
 	<gridLayout rows="250, *" columns="*" height="100%" backgroundColor="#151515">
 		<Screen />
-		<Buttons on:number={(event)=>(alert(event.detail))} on:operand={()=>(alert("Operande"))} />
+		<Buttons />
 	</gridLayout>
 </page>
